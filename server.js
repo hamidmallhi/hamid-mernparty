f6134bcd3a56d1b8a85de09c60c756515490a522
hamidmallhi <hamidansarullah@hotmail.com>
2020-05-28 10:29:23 -0600

    make it better

--- server.js
+++ server.js
@@ -14,8 +14,15 @@ app.use('/guests', require('./routes/guests'))
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-  }
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
